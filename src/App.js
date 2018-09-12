17dd0171c2f98d8f411a3e597c1f7715a5a8bec7
Digvijay Upadhyay <digvijay.upadhyay@globant.com>
2018-09-12 16:54:40 +0100

    added index html page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SomePage from './pages/SomePage';
-import { Link } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
@@ -11,18 +11,9 @@ class App extends Component {
       <HashRouter>
         <div>
           <header>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/somepage">Roster</Link>
-                </li>
-              </ul>
-            </nav>
+            <Navigation />
           </header>
-          <div>
+          <div className="mt-5">
             <main>
               <Switch>
                 <Route exact path="/" component={Home} />
